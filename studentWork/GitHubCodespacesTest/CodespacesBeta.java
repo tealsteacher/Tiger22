4866ca89a0ad94176e3b26dd41921462cf3b2f14
tealsteacher <darsch@ephrataschools.org>
2022-01-06 22:53:19 +0000

    Modified CodespacesTest.java

---
+++ studentWork/GitHubCodespacesTest/CodespacesBeta.java
@@ -0,0 +1,9 @@
+package studentWork.GitHubCodespacesTest;
+
+public class CodespacesBeta {
+    public static void main(String[] args) {
+        String x = "Its a New Year !!!";
+        System.out.println(x);
+        
+    }
+}
