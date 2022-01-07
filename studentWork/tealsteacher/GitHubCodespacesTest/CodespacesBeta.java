cd692d15067df8918231d62ca25a1a33fcd72a66
tealsteacher <darsch@ephrataschools.org>
2022-01-07 18:39:45 +0000

    assignent

---
+++ studentWork/tealsteacher/GitHubCodespacesTest/CodespacesBeta.java
@@ -0,0 +1,9 @@
+package studentWork.tealsteacher.GitHubCodespacesTest;
+
+public class CodespacesBeta {
+    public static void main(String[] args) {
+        String x = "2022 is the Year of the TIGER!";
+        System.out.println(x);
+        
+    }
+}
