8ee495892f3e4f143e663cb56fb78ab0948460ba
tealsteacher <darsch@ephrataschools.org>
2022-01-09 22:46:05 +0000

    tempsolved

---
+++ studentWork/tealsteacher/Arrays/EphrataTemp.java
@@ -0,0 +1,34 @@
+public class EphrataTemp {
+
+    public static void main(String[] args) {
+
+        int[] hightemp = {26, 43, 28, 30};
+
+        int sum = 0;
+        int count = 0;
+
+        // Calculate Sum, Count, and Average Temperature
+
+        for (int t = 0; t < hightemp.length; t++) {        
+            sum += hightemp[t];
+            count++;
+            //System.out.println(count);
+        }
+
+        double averageTemp = (double)sum / count;
+
+        System.out.println("Average Daily Temperature: " + averageTemp);
+
+        // Count and print out the number of days the tempature was below the average tempature
+        int badt = 0;
+        for (int t = 0; t < hightemp.length; t++) { 
+            if(hightemp[t] < averageTemp){
+                badt++;
+            }
+        }
+        
+        System.out.println("Days below Average Daily Temperature: " + badt);
+
+    }
+
+}
