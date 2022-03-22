05faa5cbd9edec8b68221b1a64b3f5e494af3b89
tealsteacher <90421402+tealsteacher@users.noreply.github.com>
2022-03-22 02:07:31 +0000

    animalclass

---
+++ studentWork/tealsteacher/Inheritance/Animals/Falcon.java
@@ -0,0 +1,7 @@
+//package studentWork.tealsteacher.Inheritance.Animals;
+
+public class Falcon extends Bird {
+   public Falcon() {
+      super("rabbit", 240.0);
+   }
+}
---
+++ studentWork/tealsteacher/Inheritance/Animals/Goose.java
@@ -0,0 +1,7 @@
+//package studentWork.tealsteacher.Inheritance.Animals;
+
+public class Goose extends Bird {
+   public Goose() {
+      super("seeds", 85.0);
+   }
+}
