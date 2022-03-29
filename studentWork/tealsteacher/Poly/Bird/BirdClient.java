3bc0d9a2754ed5e207807ffd7ef34456ce8b1c25
tealsteacher <90421402+tealsteacher@users.noreply.github.com>
2022-03-29 15:59:44 +0000

    polyexamples

---
+++ studentWork/tealsteacher/Poly/Bird/BirdClient.java
@@ -0,0 +1,18 @@
+//package studentWork.tealsteacher.Poly.Bird;
+
+public class BirdClient {
+    
+    public static void main(String[] args) {
+
+        // example of Overriding a method
+        Bird b = new Bird();
+        b.shoot();
+
+        LarryBird l = new LarryBird();
+        l.shoot();
+
+        SueBird s = new SueBird();
+        s.shoot();
+
+    }
+}
